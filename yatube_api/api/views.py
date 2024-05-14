ef75e6ce849daa2cf815a7c1ddb98ad0a182b03b
marisgan <marisgan@gmail.com>
2024-05-14 19:21:32 +0300

    fix pep8

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, mixins, permissions, serializers, viewsets
+from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import AuthorOrReadOnly
